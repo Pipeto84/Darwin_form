5050d7d4fbdc3bf382ff05c9755eb5550fabced7
Pipe Jara <pipejarauribeboteroa@gmail.com>
2024-10-17 20:23:42 -0500

    antd installed

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,16 @@ import {
   ltrCssLoader,
   rtlCssLoader,
 } from "@react-form-builder/components-rsuite";
-import { FormBuilder, BuilderView } from "@react-form-builder/designer";
+import {
+  FormBuilder,
+  BuilderView,
+} from "@react-form-builder/designer";
 import { BiDi } from "@react-form-builder/core";
 import data from "./Data/data.json";
 
-const compnentsMetada = rSuiteComponents.map((e) => e.build());
+const componentsMetada = rSuiteComponents.map((e) => e.build());
 
-const builderView = new BuilderView(compnentsMetada)
+const builderView = new BuilderView(componentsMetada)
   .withErrorMeta(rsErrorMessage.build())
   .withTooltipMeta(rsTooltip.build())
   .withViewerWrapper(RsLocalizationWrapper)
@@ -27,7 +30,6 @@ async function getFormFn(name?: string) {
   if (name === formName) return emptyForm;
   throw new Error(`El formulario ${name} no se encontro`);
 }
-
 function App() {
   return (
     <FormBuilder view={builderView} getForm={getFormFn} formName={formName} />

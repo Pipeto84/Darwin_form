0d895741d23fa7a887c9545b283363523df4ec89
Pipe Jara <pipejarauribeboteroa@gmail.com>
2024-10-21 20:33:06 -0500

    solo componentes

--- src/App.tsx
+++ src/App.tsx
@@ -9,16 +9,18 @@ import {
 import {
   FormBuilder,
   BuilderView,
+  type CustomizationMap,
 } from "@react-form-builder/designer";
 import { BiDi } from "@react-form-builder/core";
 import data from "./Data/data.json";
-import { myInput } from './components/MyInput'
+import { myInput } from "./components/MyInput";
 
 const componentsMetada = rSuiteComponents.map((e) => e.build());
 
 const builderView = new BuilderView([...componentsMetada, myInput.build()])
   .withErrorMeta(rsErrorMessage.build())
   .withTooltipMeta(rsTooltip.build())
+  .withTemplates([])
   .withViewerWrapper(RsLocalizationWrapper)
   .withCssLoader(BiDi.LTR, ltrCssLoader)
   .withCssLoader(BiDi.RTL, rtlCssLoader);
@@ -31,9 +33,27 @@ async function getFormFn(name?: string) {
   if (name === formName) return emptyForm;
   throw new Error(`El formulario ${name} no se encontro`);
 }
+
+const simpleCustomization: CustomizationMap = {
+  Header: {
+    hidden: true,
+  },
+  LeftPanel_Nav: {
+    hidden: true,
+  },
+  RightPanel: {
+    hidden: true,
+  },
+};
+
 function App() {
   return (
-    <FormBuilder view={builderView} getForm={getFormFn} formName={formName} />
+    <FormBuilder
+      view={builderView}
+      getForm={getFormFn}
+      formName={formName}
+      customization={simpleCustomization}
+    />
   );
 }
 
